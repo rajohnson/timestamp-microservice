7269d00fbd42f96c8382bf760a01f18ce24236c2
Rob Johnson <rajohnson@users.noreply.github.com>
2022-01-06 06:32:20 +0000

    handle errors and take argument

--- index.js
+++ index.js
@@ -2,11 +2,35 @@ var express = require('express');
 var app = express();
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000.')
+    console.log('Listening on port 3000.');
 });
 
-app.get('/api/', (req, res) => {
-  const utc = new Date().toUTCString()
-  const ts = new Date().getTime()
-  res.send({unix: ts, utc: utc});
-});
+app.use((req, res, next) => {
+  req.requestTime = Date();
+  next();
+})
+
+function sendTime(req, res) {
+  console.log('sending')
+  res.send({unix: req.ts, utc: req.utc});
+}
+
+app.get('/api/:time', (req, res, next) => {
+  console.log(new Date(req.params.time))
+  if(isNaN(new Date(req.params.time))) {
+    console.log('param is invalid')
+    res.send({error: 'Invalid Date'});
+  } else {
+    console.log('processing')
+    req.ts = new Date(req.params.time).getTime()
+    req.utc = new Date(req.params.time).toUTCString()
+    next()
+  }
+}, sendTime);
+
+app.get('/api', (req, res, next) => {
+  console.log('no param provided')
+  req.ts = new Date().getTime()
+  req.utc = new Date().toUTCString()
+  next()
+}, sendTime);
